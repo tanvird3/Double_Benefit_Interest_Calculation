0d370fe3217b2673d10aa5f90014bbdba75797e2
Tanvir Khan <tanvir.d3@gmail.com>
2019-12-16 17:11:34 +0600

    16-12-2019, Maturity Function Added

--- DBDS.py
+++ DBDS.py
@@ -3,6 +3,13 @@ import numpy as np
 import math
 import scipy as sc
 
+# fv=pv*(1+i)^n
+# 2=1*(1+i/m)^nm
+# ln(2)=nm*ln(1+i/m)----i
+# ln(2)/nm=ln(1+i/m)
+# exp(ln(2)/nm)=1+i/m
+# i=m*(exp(ln(2)/nm)-1)
+
 # this function calculates the required interest rate for doubling any amount in any
 # given period
 ## n is the given time in month and c is the compounding frequency in a year
@@ -21,3 +28,19 @@ def doublebenefit(duration, compounding_freq, initial_deposit):
     print("The Future Value will be {:.2f}".format(future_value))
     
 doublebenefit(120, 1, 100000)
+
+# or if we need to get the duration for a give rate
+# from equation i
+# n=ln(2)/(m*ln(1+i/m))
+def doublebenefit_maturity(compounding_freq, initial_deposit, interest_rate):
+    duration = math.log(2) / (
+        compounding_freq * math.log(1 + interest_rate / compounding_freq)
+    ) * 12
+    duration = math.ceil(duration)
+    print(
+        "The Time Required for Doubling the Amount is {:.2f}".format(duration)
+        + " "
+        + "Months"
+    )
+
+doublebenefit_maturity(1, 100000, 0.0718)
--- DBDS_FUNCTION.R
+++ DBDS_FUNCTION.R
@@ -1,15 +1,18 @@
 library(FinCal)
+options(scipen=999)
 
 # fv=pv*(1+i)^n
 # 2=1*(1+i/m)^nm
-# ln(2)=nm*ln(1+i/m)
+# ln(2)=nm*ln(1+i/m)----i
 # ln(2)/nm=ln(1+i/m)
 # exp(ln(2)/nm)=1+i/m
 # i=m*(exp(ln(2)/nm)-1)
 
-dbds_rate <- 
-  function(duration, compounding_freq, initial_deposit) { 
-    
+
+dbds_rate <-
+  function(duration,
+           compounding_freq,
+           initial_deposit) {
     duration <- duration / 12
     
     # the required interest rate
@@ -32,3 +35,18 @@ dbds_rate <-
   }
 
 dbds_rate(120, 1, 100000)
+
+# or if we need to get the duration for a give rate
+# from equation i
+# n=ln(2)/(m*ln(1+i/m))
+
+dbds_maturity <-
+  function(int_rate,
+           compounding_freq,
+           initial_deposit) {
+    maturity <- log(2) / (compounding_freq * log(1 + int_rate / compounding_freq)) * 12
+    maturity <- ceiling(maturity)
+    return(maturity)
+  }
+
+dbds_maturity(.07184955, 1, 100000)
